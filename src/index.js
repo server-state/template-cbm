9e8e7befcbd74544f48b7efb66881f686846f2df
Ludwig <richter@fliegwerk.com>
2020-01-18 00:19:39 +0100

    Changed file structure and metadata to meet new specifications

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import MyCBM from './my-cbm';
 
-const info = {
-    name: "my simple CBM",
-    version: "v0.0.9", // optional
-    description: "This is a template CBM", // optional
-    about: "server-state", // optional
-    // logoUrl: "/path/to/logo or base64" // optional
-};
-
 export default {
     component: MyCBM,
-    info
+    // logoUrl: "/path/to/logo or base64" // optional
 };
--- tests/cbm.test.js
+++ tests/cbm.test.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { create } from "react-test-renderer";
-import CBM from "../src";
 
+import CBM from "../src";
 
 test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
 });
 
 describe("CBM specification tests", () => {
-    it("should contain all necessary infos", () => {
-        expect(CBM.info).toHaveProperty("name");
-    });
-
     it("should render without any complications", () => {
         console.error = jest.fn();
         const mockData = "Test data";
